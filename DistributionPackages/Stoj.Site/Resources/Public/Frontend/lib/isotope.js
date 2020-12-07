614fa19ef2b4ab400dc0e059220e5b9450c19d0d
Rasmus Andersen <rasmus@growupstudio.dk>
2020-12-07 10:50:13 +0100

    sections

--- DistributionPackages/Stoj.Site/Resources/Public/Frontend/lib/isotope.js
+++ DistributionPackages/Stoj.Site/Resources/Public/Frontend/lib/isotope.js
@@ -3,6 +3,7 @@ var grid = document.querySelector('.gallery');
 var iso = new Isotope( grid, {
   // options...
   itemSelector: '.gallery-item',
+  stagger: 30,
   masonry: {
     columnWidth: '.gallery-item',
 	horizontalOrder: true,
@@ -14,3 +15,38 @@ imagesLoaded( grid ).on( 'progress', function() {
   // layout Isotope after each image loads
   iso.layout();
 });
+
+
+
+// bind filter button click
+var filtersElem = document.querySelector('.filter-button-group');
+filtersElem.addEventListener( 'click', function( event ) {
+  // only work with buttons
+  if ( !matchesSelector( event.target, 'button' ) ) {
+    return;
+  }
+  var filterValue = event.target.getAttribute('data-filter');
+  // use matching filter function
+  iso.arrange({ filter: filterValue });
+});
+
+
+
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.button-group');
+for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+  var buttonGroup = buttonGroups[i];
+  radioButtonGroup( buttonGroup );
+}
+
+function radioButtonGroup( buttonGroup ) {
+  buttonGroup.addEventListener( 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+    event.target.classList.add('is-checked');
+  });
+}
--- DistributionPackages/Stoj.Site/Resources/Public/Frontend/lib/main.js
+++ DistributionPackages/Stoj.Site/Resources/Public/Frontend/lib/main.js
@@ -54,23 +54,6 @@ animateIn.forEach(function (elem) {
 	});
 });
 
-//	let gallery = document.querySelectorAll('.gallery-fade');
-
-gallery.forEach(function (elem) {
-	gsap.from(elem, {
-		scrollTrigger: {
-			trigger: elem,
-			toggleActions: "play none play reverse",
-			//markers: true,
-			start: 'top center'
-		},
-		opacity: .3,
-		duration: .5,
-		delay: .1,
-		ease: "power2.out"
-	});
-});
-
 var animateText = document.querySelectorAll('.aos-text');
 
 animateText.forEach(function (elem) {
